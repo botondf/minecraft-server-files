440634cde58e834e96f75e9b8e4d95f8efbeb93f
Botond Fasanga <botond@fasanga.com>
2024-04-30 03:17:53 -0400

    fixed jar move path (did not exist)

--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ../scripts/build #change dir to server dir to place server jar there
 java -jar BuildTools.jar --rev latest
-mv spigot*.jar ../../runtime/spigot.jar
+mv spigot*.jar ../runtime
 # SET REAL PATH
