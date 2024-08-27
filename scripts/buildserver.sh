cf96d9d3ff4612c4ea4ba5213128e88be2ea5679
Botond Fasanga <botond@fasanga.com>
2024-08-27 14:38:17 -0400

    server build will not run server twice as docker compose would hang

--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -4,10 +4,9 @@ java -jar BuildTools.jar --rev latest
 mv spigot*.jar ../../runtime/
 
 cd ../../runtime || exit
-#java -Xms2G -Xmx6G -XX:+UseG1GC -jar spigot*.jar nogui
 java -XX:+UseG1GC -jar spigot*.jar nogui
 sed -i "s/false/true/g" eula.txt
-java -XX:+UseG1GC -jar spigot*.jar
-passwd=$(cat ../scripts/etc/rcon-passwd)
-sed -i "s/rcon.password=/rcon.password=$passwd/g" server.properties
-./scripts/stop.sh
+
+## remove:
+#passwd=$(cat ../scripts/etc/rcon-passwd)
+#sed -i "s/rcon.password=/rcon.password=$passwd/g" server.properties
