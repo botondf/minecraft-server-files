68b4f196603cacd662de2050fc674f3bb1c7d58b
Botond Fasanga <botond@fasanga.com>
2024-08-27 13:43:42 -0400

    fixing server building issues, mainly new non-Debian Java and build automation

--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd build #change dir to server dir to place server jar there
+cd build || exit #change dir to server dir to place server jar there
 java -jar BuildTools.jar --rev latest
 mv spigot*.jar ../../runtime/
-# SET REAL PATH
+
+cd ../../runtime || exit
+#java -Xms2G -Xmx6G -XX:+UseG1GC -jar spigot*.jar nogui
+java -XX:+UseG1GC -jar spigot*.jar nogui
+sed -i "s/false/true/g" eula.txt
+java -XX:+UseG1GC -jar spigot*.jar
+passwd=$(cat ../scripts/etc/rcon-passwd)
+sed -i "s/rcon.password=/rcon.password=$passwd/g" server.properties
+./scripts/stop.sh
