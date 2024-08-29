e7d9e50f4e74a2810ecc6b5293a81c94e0a71f35
Botond Fasanga <botond@fasanga.com>
2024-08-28 21:18:29 -0400

    update install script

--- install.sh
+++ install.sh
@@ -9,13 +9,33 @@ rm jdk-21_linux-x64_bin.deb
 apt-get clean
 
 useradd minecraft -m
-
-cd /home/minecraft || exit
+cd minecraft || exit
 git clone https://github.com/botondf/minecraft-server-files.git server
+cd server || exit
+git clone https://github.com/Tiiffi/mcrcon.git server/scripts/rcon
+make server/scripts/rcon/mcrcon
 
-cd /home/minecraft/server || exit
-git clone https://github.com/Tiiffi/mcrcon.git scripts/rcon
-make scripts/rcon/mcrcon
+cd systemd/services || exit
+systemctl enable mc.service mc-backup.service mc-backup-upload.service mc-ip.service mc-stop.service
+systemctl daemon-reload
+cd systemd/timers || exit
+systemctl enable mc.timer mc-backup.timer mc-ip.timer mc-stop.timer
+systemctl daemon-reload
+cd systemd/services || exit
+systemctl start mc.service mc-backup.service mc-backup-upload.service mc-ip.service
+systemctl daemon-reload
+cd systemd/timers || exit
+systemctl start mc.timer mc-backup.timer mc-ip.timer mc-stop.timer
+systemctl daemon-reload
+
+cd build || exit
+curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar --rev latest
+mv spigot*.jar ../../runtime/
+
+cd ../../runtime || exit
+java -XX:+UseG1GC -jar spigot*.jar nogui
+sed -i "s/false/true/g" eula.txt
 
 cd /home/minecraft || exit
 chown -R minecraft:minecraft .
@@ -23,24 +43,6 @@ chown -R minecraft:minecraft .
 cd /home/minecraft/server/scripts || exit
 chmod u+x *.sh
 
-cd /home/minecraft/server/systemd/services || exit
-systemctl enable mc.service mc-backup.service mc-backup-upload.service mc-ip.service mc-stop.service
-systemctl daemon-reload
-
-cd /home/minecraft/server/systemd/timers || exit
-systemctl enable mc.timer mc-backup.timer mc-ip.timer mc-stop.timer
-systemctl daemon-reload
-
-cd /home/minecraft/server/systemd/services || exit
-systemctl start mc.service mc-backup.service mc-backup-upload.service mc-ip.service
-systemctl daemon-reload
-
-cd /home/minecraft/server/systemd/timers || exit
-systemctl start mc.timer mc-backup.timer mc-ip.timer mc-stop.timer
-systemctl daemon-reload
-
 su minecraft
-cd /home/minecraft/server/scripts || exit
-./buildtools.sh
-./buildserver.sh
-cd /home/minecraft/server || exit
+
+exit
