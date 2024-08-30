5af562d53432b1ba8e9d1f72a3cbd7306380cd27
Botond Fasanga <botond@fasanga.com>
2024-08-30 12:43:39 -0400

    systemctl enables had wrong working dir, removed bad cd's + instructions at top for running

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# ATTENTION:
+# this script is meant to be run as root
+# *MUST* be run from the git repo directory. please cd into to the git directory, whatever name was set during cloning
+# Set executable permissions on install script, install.sh, located in the repo root, i.e. directory mentioned before:
+# chmod u+x install.sh
+
 grep https://github.com/botondf/ .git/config || exit
 DIR=$(pwd) # get git repo dir path
 #NAME=$(basename "$DIR") # get git repo dir name
@@ -27,17 +33,17 @@ cp systemd/timers/mc*.timer /etc/systemd/system
 cd /etc/systemd/system || exit
 systemctl enable mc.service mc-backup.service mc-backup-upload.service mc-ip.service mc-stop.service
 systemctl daemon-reload
-cd systemd/timers || exit
+#cd systemd/timers || exit
 systemctl enable mc.timer mc-backup.timer mc-ip.timer mc-stop.timer
 systemctl daemon-reload
-cd systemd/services || exit
+#cd systemd/services || exit
 systemctl start mc.service mc-backup.service mc-backup-upload.service mc-ip.service
 systemctl daemon-reload
-cd systemd/timers || exit
+#cd systemd/timers || exit
 systemctl start mc.timer mc-backup.timer mc-ip.timer mc-stop.timer
 systemctl daemon-reload
 
-cd build || exit
+cd /home/minecraft/server/scripts/build || exit
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar --rev latest
 mv spigot*.jar ../../runtime/
