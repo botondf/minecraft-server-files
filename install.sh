8cd76f029bd35908cc2fe6607b461c016f552bdc
Botond Fasanga <botond@fasanga.com>
2024-09-13 11:59:46 -0400

    temporarily disabled most of install, fixed issue with .config/systemd, now script makes that dir

--- install.sh
+++ install.sh
@@ -21,20 +21,23 @@ cd /home/minecraft || exit
 mv "$DIR"/runtime /home/minecraft/runtime || exit
 mv "$DIR" /home/minecraft/ || exit
 
-apt-get update
-apt-get dist-upgrade -y
-apt-get install -y curl gcc make vim zip unzip rclone
-curl -O https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
-dpkg -i jdk-21_linux-x64_bin.deb
-rm jdk-21_linux-x64_bin.deb
-apt-get clean
+# temp disable
+#apt-get update
+#apt-get dist-upgrade -y
+#apt-get install -y curl gcc make vim zip unzip rclone
+#curl -O https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+#dpkg -i jdk-21_linux-x64_bin.deb
+#rm jdk-21_linux-x64_bin.deb
+#apt-get clean
 
 # /server was moved
 # cd server || exit
 cd /home/minecraft || exit
-git clone https://github.com/Tiiffi/mcrcon.git scripts/rcon
-make scripts/rcon/mcrcon
+# temp disable
+#git clone https://github.com/Tiiffi/mcrcon.git scripts/rcon
+#make scripts/rcon/mcrcon
 
+mkdir /home/minecraft/.config/systemd/user
 cp systemd/services/mc*.service /home/minecraft/.config/systemd/user/
 cp systemd/timers/mc*.timer /home/minecraft/.config/systemd/user/
 
@@ -52,20 +55,20 @@ systemctl start mc.timer mc-backup.timer mc-ip.timer mc-stop.timer
 systemctl daemon-reload
 
 cd /home/minecraft/scripts/build || exit
-curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-java -jar BuildTools.jar --rev latest
-mv spigot*.jar ../../runtime/
+# temp disable
+#curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+#java -jar BuildTools.jar --rev latest
+#mv spigot*.jar ../../runtime/
 
 cd ../../runtime || exit
-java -XX:+UseG1GC -jar spigot*.jar nogui
-sed -i "s/false/true/g" eula.txt
+# temp disable
+#java -XX:+UseG1GC -jar spigot*.jar nogui
+#sed -i "s/false/true/g" eula.txt
 
 cd /home/minecraft || exit
 chown -R minecraft:minecraft .
 
 cd /home/minecraft/server/scripts || exit
 chmod u+x *.sh
-
-su minecraft
 
 exit
